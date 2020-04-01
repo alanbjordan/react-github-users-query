116ba81e51d977f282e01474bded920e37641984
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-01 12:59:28 -0400

    Refactoring -Removed imported logo -Imported UserCardList -Reomved functional component code -Created a class component -Added render function -Added handle functions with fetch -Added input form with submit button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import UserCardList from './components/UserCardList';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    textInput: "",
+    users: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      textInput: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const userName = this.state.textInput;
+    fetch(`https://api.github.com/users/${userName}`)
+    .then(res => res.json())
+    .then((result) => {
+        this.setState({
+            textInput: "",
+            users: [...this.state.users,result]
+        });
+    });
+  };
+
+  render() {
+    const {users} = this.state;
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter user Name:
+            <input
+              type="text"
+              value={this.state.textInput}
+              placeholder="Text Input"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <UserCardList 
+          users={users}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
